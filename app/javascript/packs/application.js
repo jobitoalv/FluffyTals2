072a25b5f7936b33d6bc286d4749272a38acc55b
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-08-09 15:49:46 +1000

    changed from link_to to button_to on delete funtion

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,8 +14,12 @@
 // Rails.start()
 // Turbolinks.start()
 // ActiveStorage.start()
-
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 //= require jquery3
+//= require jquery_ujs
 //= require popper
 //= require bootstrap
 
