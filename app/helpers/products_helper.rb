5b739a9e3be791a14682acc97815fd28a9c52017
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-08-09 13:55:53 +1000

    added payment

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,7 @@
 module ProductsHelper
      def render_buy_button(current_user, product)
         if current_user
-             button = "<button data-stripe=\"payment\" class='btn btn-primary p-3'> Buy: $#{post.price}</button>"
+             button = "<button data-stripe=\"payment\" class='btn btn-primary p-3'> Buy: $#{product.price}</button>"
         else
              button = "<button class=\"btn btn-primary\" disabled>Log In To Buy</button>"
         end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'item/page'
+  get "payments/page" , to: "payments#page" , as: 'payment_page'
   devise_for :users 
   root 'home#page'
   resources :products
   get "payments/success", to: "payments#success"
-  post "payments/webhook", to: "payments#webhook"
+  #post "payments/webhook", to: "payments#webhook"
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
