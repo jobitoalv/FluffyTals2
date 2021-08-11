f3f1a912645738fd8eec6bbd10139a074f475fdb
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-08-11 22:45:40 +1000

    added rails

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@
  import * as ActiveStorage from "@rails/activestorage"
 // import "channels"
 
-// import "bootstrap"
-// require("@rails/ujs").start()
+ import "bootstrap"
+ require("@rails/ujs").start()
 
  Rails.start()
  Turbolinks.start()
