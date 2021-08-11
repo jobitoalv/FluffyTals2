b52eda11b10ca58d0cec3717bad226d601d85436
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-08-11 15:01:26 +1000

    added config

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module FluffyTals2
     config.load_defaults 6.0
     config.serve_static_assets = true
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
